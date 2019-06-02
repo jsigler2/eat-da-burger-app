0a0313e12ea522b4587caf31c7fbf140f5ef3849
Jenna Sigler <jennasigler@Jennas-MacBook-Pro.local>
2019-06-01 22:14:25 -0700

    committing burger-app

---
+++ db/seeds.sql
@@ -0,0 +1,32 @@
+INSERT INTO burgers
+    (burger_name)
+VALUES
+    ('Double Double');
+INSERT INTO burgers
+    (burger_name)
+VALUES
+    ('The Whopper');
+INSERT INTO burgers
+    (burger_name)
+VALUES
+    ('Vegan Burger');
+INSERT INTO burgers
+    (burger_name)
+VALUES
+    ('Turkey Burger');
+INSERT INTO burgers
+    (burger_name)
+VALUES
+    ('The Hangover Burger');
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('Black Bean Burger', true);
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('Protein-Style Burger', true);
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('Cheeseburger', true);
